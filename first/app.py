047c11ac997965467cc18808e63c4b79c82bd8e6
chuiizeet <chuy.guerra19@outlook.es>
2019-06-09 16:12:46 -0500

    endpoints logic

--- first/app.py
+++ first/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -17,27 +17,51 @@ stores = [
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-   pass
+	request_data = request.get_json()
+	new_store = {
+		'name': reques_data['name'],
+		'items': []
+	}
+	stores.append(new_store)
+   	return jsonify(new_store)
 
 # GET /store/<string:name>
 @app.route('/stote/<string:name>') 
 def get_store(name):
-   pass
-
+   	for store in stores:
+		if store['name'] == name:
+			return jsonify(store)
+		else:
+			return jsonify({'message': 'store not found'})
+			
 # GET /store
 @app.route('/store')
 def get_stores():
-   return jsonify({'stores': stores}) 
+   	return jsonify({'stores': stores}) 
 
 # POST /store/<string:name>/item {name:, price}
 @app.route('/stotr/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-   pass 
+   	request_data = request.get_json()
+   	for store in stores:
+			if store['name'] == name:
+			new_item = {
+				'name': request_data['name'],	
+				'price': request_data['price']
+			}
+			store['items'].append(new_item)
+			return jsonify(new_item)
+	return jsonify({'message': 'store not found'})
 
 # GET /store/<string:name>/item
 @app.route('/route/<string:name>/item')
 def get_items_in_store(name):
-   pass
+   	for store in stores:
+		if store['name'] == name:
+			return jsonify({'items': store['items']})
+		else:
+			return jsonify({'message': 'store not found'})
+
 
 
 
