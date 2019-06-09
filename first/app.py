961413bbbd7f857945e82716f643f49ea720eb58
chuiizeet <chuy.guerra19@outlook.es>
2019-06-09 15:29:03 -0500

    endpointa

--- first/app.py
+++ first/app.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+   pass
 
-    return 'ROFL'
+# GET /store/<string:name>
+@app.route('/stote/<string:name>', methods=['GET', 'POST'])
+def method_name():
+   pass
+
+
+
+
 
 app.run(port=5000)
