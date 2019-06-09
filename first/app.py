4d5d97f708ca83e7c9b673fe5dd3f1dc37e8b418
chuiizeet <chuy.guerra19@outlook.es>
2019-06-09 15:42:24 -0500

    Test jsonify

--- first/app.py
+++ first/app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+stores = [
+	{
+		'name': 'Tianguis',
+		'items': [
+			{
+				'name': 'Mona china',
+				'price': 500
+			}
+		]
+	}
+]
 
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
@@ -8,8 +20,23 @@ def create_store():
    pass
 
 # GET /store/<string:name>
-@app.route('/stote/<string:name>', methods=['GET', 'POST'])
-def method_name():
+@app.route('/stote/<string:name>') 
+def get_store(name):
+   pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+   return jsonify({'stores': stores}) 
+
+# POST /store/<string:name>/item {name:, price}
+@app.route('/stotr/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+   pass 
+
+# GET /store/<string:name>/item
+@app.route('/route/<string:name>/item')
+def get_items_in_store(name):
    pass
 
 
