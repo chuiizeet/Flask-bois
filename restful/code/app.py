19d8f4acd787ba9e95d0e675bd742951e0b5661e
chuiizeet <chuy.guerra19@outlook.es>
2019-06-10 01:07:20 -0500

    get, post, test postman

--- restful/code/app.py
+++ restful/code/app.py
@@ -4,10 +4,21 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-	def get(self, name):
-		return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
+class Item(Resource):
+	def get(self, name):
+		for item in items:
+			if item['name'] == name:
+				return item
+		return {'item': None}, 404
+		
+	
+	def post(self, name):
+		item = {'name': name, 'price': 12.00}
+		items.append(item)
+		return item, 201
+
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000)
