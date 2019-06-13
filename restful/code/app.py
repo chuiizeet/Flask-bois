65aed39ecf64124cea2b269738042e390ee37c85
chuiizeet <chuy.guerra19@outlook.es>
2019-06-13 17:36:37 -0500

    Error control

--- restful/code/app.py
+++ restful/code/app.py
@@ -8,13 +8,13 @@ items = []
 
 class Item(Resource):
 	def get(self, name):
-		for item in items:
-			if item['name'] == name:
-				return item
-		return {'item': None}, 404
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		return {'item': None}, 200 if item else 404
 		
-	
 	def post(self, name):
+		if next(filter(lambda x: x['name'] == name, items), None):
+			return {'message': "An item with name '{}' already exists. ".format(name)}, 400
+
 		data = request.get_json()
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
