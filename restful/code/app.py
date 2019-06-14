b368794c33f5fb8aaff4b78320dd344a83956dc5
chuiizeet <chuy.guerra19@outlook.es>
2019-06-14 00:18:59 -0500

    Parser with reqparse

--- restful/code/app.py
+++ restful/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from secure import authenticate, identity
@@ -33,7 +33,14 @@ class Item(Resource):
 		return {'message': 'Item deleted'}
 
 	def put(self, name):
-		data = request.get_json()
+		parser = reqparse.RequestParser()
+		parser.add_argument('price',
+			type=float,
+			required=True,
+			help="This field cannot be left blank!"
+		)
+		data = parser.parse_args()
+		
 		item = next(filter(lambda x: x['name'] == name, items ), None)
 		if item is None:
 			item = {'name': name, 'price': data['price']}
