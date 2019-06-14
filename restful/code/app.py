f62698a6f10f83f4afd9e518c69dcc6f421f707e
chuiizeet <chuy.guerra19@outlook.es>
2019-06-13 19:13:16 -0500

    Delete item

--- restful/code/app.py
+++ restful/code/app.py
@@ -27,6 +27,11 @@ class Item(Resource):
 		items.append(item)
 		return item, 201
 
+	def delete(self, name):
+		global items
+		items = list(filter(lambda x: x['name'] != name, items))
+		return {'message': 'Item deleted'}
+
 class ItemList(Resource):
 	def get(self):
 		return {'items': items}
