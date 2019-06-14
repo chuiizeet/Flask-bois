5f27dc6fbde5e070cae387181c223c0e8788dd10
chuiizeet <chuy.guerra19@outlook.es>
2019-06-13 19:27:51 -0500

    Put item

--- restful/code/app.py
+++ restful/code/app.py
@@ -32,6 +32,16 @@ class Item(Resource):
 		items = list(filter(lambda x: x['name'] != name, items))
 		return {'message': 'Item deleted'}
 
+	def put(self, name):
+		data = request.get_json()
+		item = next(filter(lambda x: x['name'] == name, items ), None)
+		if item is None:
+			item = {'name': name, 'price': data['price']}
+			items.append(item)
+		else:
+			item.update(data)
+		return item
+
 class ItemList(Resource):
 	def get(self):
 		return {'items': items}
