2397a1aeaed441e72414ff6ab2dbe694c7f6c92b
chuiizeet <chuy.guerra19@outlook.es>
2019-06-16 01:09:29 -0500

    Sign up users db

--- database-test/code/app.py
+++ database-test/code/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from secure import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'chuy'
@@ -55,5 +56,6 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000, debug=True)
--- database-test/code/user.py
+++ database-test/code/user.py
@@ -1,4 +1,6 @@
 import sqlite3
+from flask_restful import Resource, reqparse
+
 
 class User:
 	def __init__(self, _id, username, password):
@@ -39,3 +41,33 @@ class User:
 
 		conenction.close()
 		return user
+
+class UserRegister(Resource):
+
+	parser = reqparse.RequestParser()
+	parser.add_argument('username',
+		type=str,
+		required=True,
+		help="This field cannot be blank."
+	)
+
+	parser.add_argument('password',
+		type=str,
+		required=True,
+		help="This field cannot be blank."
+	)
+
+	def post(self):
+		data = UserRegister.parser.parse_args()
+
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+ 		
+		query = "INSERT INTO users VALUES (NULL, ?, ?)"
+		cursor.execute(query, (data['username'], data['password']))
+
+		connection.commit()
+		connection.close()
+
+		return {'message': "User created successfully."}, 201
+
