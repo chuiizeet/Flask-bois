e5751a032bdf7c3803334bf6c2f2a885018b8f67
chuiizeet <chuy.guerra19@outlook.es>
2019-06-16 02:05:01 -0500

    Post item db

--- database-test/code/item.py
+++ database-test/code/item.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, request
 from flask_jwt import jwt_required
 
 class Item(Resource):
@@ -10,8 +10,8 @@ class Item(Resource):
 		help="This field cannot be left blank!"
 	)
 
-	@jwt_required()
-	def get(self, name):
+	@classmethod
+	def find_by_name(cls, name):
 		connection = sqlite3.connect('data.db')
 		cursor = connection.cursor()
 
@@ -22,15 +22,31 @@ class Item(Resource):
 
 		if row:
 			return {'item': {'name': row[0], 'price': row[1]}}
-		return {'message': 'Item not found'}, 404
-		
+
+	@jwt_required()
+	def get(self, name):
+		item = self.find_by_name(name)
+		if item:
+			return item
+		else:
+			return {'message': 'Item not found'}, 404
+
 	def post(self, name):
-		if next(filter(lambda x: x['name'] == name, items), None):
+		if self.find_by_name(name):
 			return {'message': "An item with name '{}' already exists. ".format(name)}, 400
 
 		data = request.get_json()
 		item = {'name': name, 'price': data['price']}
-		items.append(item)
+
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		query = "INSERT INTO items VALUES (?, ?)"
+		cursor.execute(query, (item['name'], item['price']))
+
+		connection.commit()
+		connection.close()
+
 		return item, 201
 
 	def delete(self, name):
